09a40ff8842675c07891e8939ffd5a7cb6745188
jbcostan <jbcostan@hotmail.com>
2020-06-09 21:31:41 -0400

    fixed responsive header

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,6 +1,14 @@
 import React,{useState} from 'react';
 
 function Topnav(props){
+    let mds="";
+    if(props.showMenu==false){
+        mds="w-full block flex-grow lg:flex lg:items-center lg:w-auto md:hidden sm:hidden xm:hidden";
+    }
+    else{
+        mds="w-full block flex-grow lg:flex lg:items-center lg:w-auto";
+    }
+
     return(
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -12,7 +20,7 @@ function Topnav(props){
                 <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className={mds}>
                 <div className="text-sm lg:flex-grow">
                 <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     Docs
@@ -25,7 +33,8 @@ function Topnav(props){
                 </a>
                 </div>
                 <div>
-                <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+                <a href="#" className="px-4 py-2 text-teal-100 font-semibold hover:text-white hover:font-underline">Login</a>
+                <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign Up</a>
                 </div>
             </div>
         </nav>
